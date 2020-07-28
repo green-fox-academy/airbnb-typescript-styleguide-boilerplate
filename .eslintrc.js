b1fa9d2059f583b1f8694635c95781fee9f0762f
Viktor Rádi <rviktor87@web-fejlesztes.eu>
2020-07-28 16:37:11 +0200

    extending with ignore .ts extensions and reformat

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,17 +1,27 @@
 module.exports = {
-    extends: ['airbnb-base', 'plugin:@typescript-eslint/recommended'],
-    parser: '@typescript-eslint/parser',
-    plugins: ['@typescript-eslint', 'prettier'],
-    settings: {
-        'import/parsers': {
-            '@typescript-eslint/parser': ['.ts'],
-        },
-        'import/resolver': {
-            typescript: {},
-        },
+  extends: ['airbnb-base', 'plugin:@typescript-eslint/recommended'],
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint', 'prettier'],
+  settings: {
+    'import/parsers': {
+      '@typescript-eslint/parser': ['.ts'],
     },
-    rules: {
-        'import/no-extraneous-dependencies': [2, { devDependencies: ['**/test.ts'] }],
-        '@typescript-eslint/indent': [2, 2],
+    'import/resolver': {
+      typescript: {},
     },
+  },
+  rules: {
+    'import/no-extraneous-dependencies': [2, { devDependencies: ['**/test.ts'] }],
+    '@typescript-eslint/indent': [2, 2],
+    'import/extensions': [
+      'error',
+      'ignorePackages',
+      {
+        js: 'never',
+        jsx: 'never',
+        ts: 'never',
+        tsx: 'never',
+      },
+    ],
+  },
 };
